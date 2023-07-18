97158618d53e1ba5b7f3e5c372a818c44eecfead
Tunda Srisawatpichantanit <tunda.westride@outlook.com>
2023-07-18 13:15:20 +0700

    DOM lession

--- dom.js
+++ dom.js
@@ -17,3 +17,65 @@ let color = document.querySelector(".colors");
 color.style.color = "red"
 color.style.backgroundColor = "blue"
 color.style.fontSize = "40px"
+
+
+let message = document.querySelector(".display")
+let button = document.querySelector(".button")
+
+button.addEventListener('click', (display) => {
+    console.log(message.style.display);
+
+    if (message.style.display === "none") {
+        message.style.display = "block"
+    } else {
+        message.style.display = "none"
+    }
+})
+
+function currentTime() {
+    let date = new Date()
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let session = "AM"
+
+    if (hh > 12) {
+        session = "PM"
+    }
+    //Normal
+    // if (hh < 10) {
+    //     hh = "0" + hh
+    // } else {
+    //     hh = hh
+    // }
+    //Short if
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
+
+    let time = hh + ":" + mm + ":" + ss + " " + session;
+
+    document.getElementById("clock").innerHTML = time
+
+    let t = setTimeout(function () {
+        currentTime()
+    }, 1000)
+
+}
+// currentTime();
+
+function onValidate() {
+    let text = document.getElementById("numb").value
+
+    console.log(text);
+
+    let result
+    if (text < 5) {
+        result = "value less than 5"
+    } else {
+        result = "value more than 5"
+    }
+
+    document.getElementById("result").innerHTML = result
+}

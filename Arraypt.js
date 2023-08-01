8d9dc615d5f3cdd740dcab09af7df862a45db36f
Tunda Srisawatpichantanit <tunda.westride@outlook.com>
2023-08-01 22:11:57 +0700

    Update challenge 2 and practice array loop

--- Arraypt.js
+++ Arraypt.js
@@ -1,47 +1,46 @@
 
-// Picking the fruits
+// // Picking the fruits
 
 // index              0         1        2        3           4           5       
 const testFruits = ["Papaya", "Mango", "Apple", "Lychee", "Watermalon", "Banana"]
 console.log(testFruits)
-
-let printfruits = testFruits[3]
-console.log(printfruits)
-console.log(testFruits[5])
+// let printfruits = testFruits[3]
+// console.log(printfruits)
+// console.log(testFruits[5])
 
 // Fruits loop array
 
 function runfruittest() {
 
     for (let runfruits = 0; runfruits < testFruits.length; runfruits++) {
-        console.log(testFruits[0] + " is not my favorite fruit")
+        // console.log(testFruits[0] + " is not my favorite fruit")
 
-        if (runfruits = testFruits[2]) {
-            console.log(testFruits[2] + " Nice!")
-        }
-        if (runfruits = testFruits[3]) {
-            console.log(testFruits[3] + " Yummy!!")
-        } else { console.log(testFruits + "Also Not my favorite fruit") }
+        if (runfruits === 2) {
+            console.log(testFruits[runfruits] + " Nice!")
+            // } if (runfruits === 3) { // if using this will checking again not continue
+        } else if (runfruits === 3) {
+            console.log(testFruits[runfruits] + " Yummy!!")
+        } else { console.log(testFruits[runfruits] + " is not my favorite fruit") }
     }
 
 }
 
-console.log(runfruittest())
+runfruittest()
 
 
-// // Select month name
+// Select month name
 
-// const now = new Date()
-// console.log(now)
+const now = new Date()
+console.log(now)
 
-// const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-// console.log(monthName)
+const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+console.log(monthName)
 
-// let currentmonth = monthName[now.getMonth()]
-// console.log(currentmonth)
+let currentmonth = monthName[now.getMonth()]
+console.log(currentmonth)
 
-// const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-// console.log(weekday)
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+console.log(weekday)
 
-// let currentweekday = weekday[now.getDate()]
-// console.log(currentweekday)
+let currentweekday = weekday[now.getDay()]
+console.log(currentweekday)

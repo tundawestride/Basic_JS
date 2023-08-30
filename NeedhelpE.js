16902f4313c2c9657d6a8a72c68c157aaecded06
Tunda Srisawatpichantanit <tunda.westride@outlook.com>
2023-08-30 13:03:48 +0700

    Update exercise Easy

---
+++ NeedhelpE.js
@@ -0,0 +1,88 @@
+//=====Easy part, Spotlight Sum https://edabit.com/challenge/PZ7rZh9C47CvYHfN2
+
+// This is the table example
+
+// let activities = [
+//     ['Work', 9],
+//     ['Eat', 1],
+//     ['Commute', 2],
+//     ['Play Game', 1],
+//     ['Sleep', 7]
+// ];
+
+// console.table(activities);
+// console.log(activities[2][1]); // 2
+// console.log(activities[4][1]); // 7
+
+// activities.push(['New', 4]); // push to index [5][1] or last
+// console.table(activities);
+
+// activities.splice(3, 0, ['Programming', 2]); // splice to add to index [3][0] X Y
+// console.table(activities);
+
+// activities.forEach(activities => {
+//     let percentage = ((activities[1] / 24) * 100).toFixed();
+//     activities[2] = percentage + "%"
+// })
+
+// console.table(activities);
+
+// activities.pop(); // deletet last index [6]
+
+// console.table(activities);
+
+// activities.forEach(activities => {
+//     activities.pop(2)// delete activities(2)
+// })
+// console.table(activities)
+
+// function tryforlooptable() {
+//     for (let ii = 0; ii < activities.length; ii++) {    // get the size of the inner array
+//         let innerArrL = activities[ii].length; // loop the inner array
+//         // console.log(ii) // 0 1 2 3 4 5
+//         // console.log(innerArrL) // 2 2 2 2 2 2 is 0 and 1 ['Work', 9]... length = two
+//         for (let jj = 0; jj < innerArrL; jj++) {
+//             // console.log(jj) // 0 1 0 1 0 1 0 1 run loop in innerArrL
+//             console.log('[' + ii + ',' + jj + '] = ' + activities[ii][jj]); //
+//         }
+//     }
+
+//     activities.forEach((activitys) => {
+//         activitys.forEach((data) => {
+//             console.log(data);
+//         });
+//     });
+// }
+// tryforlooptable()
+
+// Spotlight Sum
+
+// let nset = [
+//     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+//     [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+//     [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+//     [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+//     [41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+//     [51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+//     [61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
+//     [71, 72, 73, 74, 75, 76, 77, 78, 79, 80],
+//     [81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
+//     [91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+// ]
+
+// console.log(nset)
+// console.table(nset)
+
+// function whatisXYspotlite(sn) {
+//     console.log(nset[4][5]) // X4 Y5 = 46
+// }
+
+// whatisXYspotlite(46)
+
+// function spotlightSum(sn) {
+//     console.log(nset[4][5]) // X4 Y5 = 46
+// }
+
+// spotlightSum(15)
+
+//=====
---
+++ jsexerE.js
@@ -0,0 +1,64 @@
+// //https://edabit.com/challenges/javascript
+
+
+//=====1. Older Than Me https://edabit.com/challenge/iwdZiFucR5wkQsFHu
+
+// let p1 = {
+//     name: 'Sammy',
+//     Age: 40
+// }
+
+// let p2 = {
+//     name: 'Joel',
+//     Age: 36
+// }
+
+// let p3 = {
+//     name: 'Lily',
+//     Age: 24
+// }
+// let p4 = {
+//     name: 'Mariel',
+//     Age: 24
+// }
+
+// // console.log(p1.name)
+// // console.log(p1.Age)
+
+// function CompareAge(px, py) {
+//     let pxAge = px.Age
+//     let pyAge = py.Age
+//     let differage = pxAge - pyAge
+//     let resultdiffer = Math.sign(differage) //The Math.sign() static method returns 1 or -1, indicating the sign of the number passed as argument. If the input is 0 or -0, it will be returned as-is.
+//     if (resultdiffer == 1) {
+//         console.log(px.name + ' is older than me')
+//     } else if (resultdiffer == -1) {
+//         console.log(px.name + ' is younger than me')
+//     } else { console.log(px.name + ' is the same age as me') }
+
+// }
+
+// CompareAge(p2, p3)
+// CompareAge(p1, p2)
+// CompareAge(p3, p1)
+// CompareAge(p3, p4)
+
+//Q: How to make code accept in form function p2.compareAge(p1)??
+
+//=====2. Number Split https://edabit.com/challenge/xsi99TwpGyFC8KS6d
+
+let xy = []
+function numberSplit(n) {
+    let nhalf = n / 2 // 5 / 2
+    // console.log(nhalf) // 2.5
+    let x = Math.floor(nhalf)
+    // console.log(x) // 2
+    let y = Math.ceil(nhalf)
+    // console.log(y) // 3
+    xy.push(x, y) // push [ 2, 3 ]
+    console.log(xy)
+}
+numberSplit(17) // [ 8, 9 ]
+
+//=====3. 
+

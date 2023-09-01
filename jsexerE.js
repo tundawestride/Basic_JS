da833592973ec03887410187fe360a598a37b880
Tunda Srisawatpichantanit <tunda.westride@outlook.com>
2023-09-01 13:06:52 +0700

    Update excercise JS easy

--- jsexerE.js
+++ jsexerE.js
@@ -47,18 +47,135 @@
 
 //=====2. Number Split https://edabit.com/challenge/xsi99TwpGyFC8KS6d
 
-let xy = []
-function numberSplit(n) {
-    let nhalf = n / 2 // 5 / 2
-    // console.log(nhalf) // 2.5
-    let x = Math.floor(nhalf)
-    // console.log(x) // 2
-    let y = Math.ceil(nhalf)
-    // console.log(y) // 3
-    xy.push(x, y) // push [ 2, 3 ]
-    console.log(xy)
-}
-numberSplit(17) // [ 8, 9 ]
+// let xy = []
+// function numberSplit(n) {
+//     let nhalf = n / 2 // 5 / 2
+//     // console.log(nhalf) // 2.5
+//     let x = Math.floor(nhalf)
+//     // console.log(x) // 2
+//     let y = Math.ceil(nhalf)
+//     // console.log(y) // 3
+//     xy.push(x, y) // push [ 2, 3 ]
+//     console.log(xy)
+// }
+// numberSplit(17) // [ 8, 9 ]
 
-//=====3. 
+//=====3. Absolute Sum
 
+// let arrabssum = []
+
+// function getAbsSum(arr) {
+//     totalsum = 0
+//     for (let index = 0; index < arr.length; index++) {
+//         let arrelement = arr[index];
+//         // console.log(arrelement) // 4 -8 -10 9 7
+//         totalsum += Math.abs(arrelement)
+//         //let abselement = Math.abs(arrelement)
+//         // totalsum = totalsum + abselement
+//     }
+//     return totalsum
+// }
+
+// console.log(getAbsSum([4, -8, -10, 9, 7]))
+// console.log(getAbsSum([1, 2, 3, 4, 5]))
+// console.log(getAbsSum([1, -2, -3, 4, 5]))
+
+//=====4. Sort an Array by String Length
+
+// function sortByLength(arr) {
+//     let sorting = arr.sort((a, b) => a.length - b.length)
+//     return sorting
+// }
+
+// console.log(sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"]))
+// console.log(sortByLength(["Turing", "Einstein", "Jung"]))
+
+//=====5. Incorrect Import Statement
+
+//from module_name import object
+
+// function fixImport(s)
+
+//     letwords = s.split(' ')
+//     //                index          0        1        2         3
+//     // console.log(letwords) // ['import', 'object', 'from', module_name]
+//     // resequence 2 3 0 1
+//     let fixim = letwords[2] + ' ' + letwords[3] + ' ' + letwords[0] + ' ' + letwords[1]
+//     return fixim
+// }
+
+// console.log(fixImport('import object from module_name'))
+// console.log(fixImport("import randint from random"))
+// console.log(fixImport("import pi from math"))
+
+//=====6. Find the Bomb
+
+// function bomb(str) {
+//     strlowercase = str.toLowerCase()
+//     if (strlowercase.includes('bomb')) {
+//         console.log('Duck!')
+//     } else (console.log("There is no bomb, relax."))
+// }
+
+// bomb("There is a BoMb.")
+// bomb("Hey, did you think there is a bomb?.")
+// bomb("This goes boom!!!")
+
+//=====7. Tuck in Array https://edabit.com/challenge/7ysTEDruHz2prcJQ9
+
+// function tuckIn(arr1, arr2) {
+//     // console.log(arr1[0])
+//     // console.log(arr1[1])
+
+//     letarr2tuckin = [arr1[0], ...arr2, arr1[1]]
+//     return letarr2tuckin
+
+// }
+
+// console.log(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]))
+// console.log(tuckIn([15, 150], [45, 75, 35]))
+// console.log(tuckIn([[1, 2], [5, 6]], [[3, 4]]))
+
+//====8. Phone Number Formatting https://edabit.com/challenge/Z6oY6EWwT9rde8YXm
+
+// function formatPhoneNumber(numbers) {
+
+//     let joinallnums = numbers.join('')
+//     // console.log(joinallnums)
+//     // console.log(joinallnums[0])
+
+//     // Shorter method
+//     //  numbers.splice(6, 0, "-");
+//     // numbers.splice(3, 0, ") ");
+//     // numbers.splice(0, 0, "(");
+//     // return numbers.join('');
+
+//     let newformat = '"' + "(" + joinallnums[0] + joinallnums[1] + joinallnums[2] + ")" + " " + joinallnums[3] + joinallnums[4] + joinallnums[5] + "-" + joinallnums[6] + joinallnums[7] + joinallnums[8] + joinallnums[9] + '"'
+//     return newformat
+
+// }
+
+// console.log(formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+// console.log(formatPhoneNumber(([5, 1, 9, 5, 5, 5, 4, 4, 6, 8])))
+// console.log(formatPhoneNumber([3, 4, 5, 5, 0, 1, 2, 5, 2, 7]))
+
+//=====9. Learn Lodash (2): Compact https://edabit.com/challenge/M6fbYyBkzJXMAu39G
+
+// function compact(arr) {
+//     // console.log(arr)
+//     // console.log(Boolean(0)) // false
+//     // console.log(Boolean("")) // true
+//     // console.log(Boolean(1)) // true
+//     // let cutmorethan2 = arr.filter((arr) => arr > 2)
+//     // console.log(cutmorethan2) // 3
+//     let cutonlyBoolean = arr.filter(Boolean)
+//     return (cutonlyBoolean)
+// }
+
+// console.log(compact([0, 1, false, 2, "", 3]))
+// console.log(compact([5, 1, true, " + ", undefined, 3]))
+// console.log(compact([false, null, 0, "", undefined, NaN]))
+
+//=====10.
+
+//=====11.
